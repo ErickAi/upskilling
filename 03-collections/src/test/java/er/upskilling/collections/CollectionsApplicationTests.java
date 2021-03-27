3b1de16fec968786e16562f8aa8bc0d6f95af8f8
Erik Aitkulov <eaitkulov@luxoft.com>
2021-03-28 00:28:42 +0300

    collections

---
+++ 03-collections/src/test/java/er/upskilling/collections/CollectionsApplicationTests.java
@@ -0,0 +1,14 @@
+package er.upskilling.collections;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+class CollectionsApplicationTests {
+
+  @Test
+  void test() {
+    Collection<String> collection = new ArrayList<>();
+  }
+}
